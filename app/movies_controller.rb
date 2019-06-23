ed7446dd9ec6a27b03b9cdb6f153f9bfa8f054ee
M Banks <montensia1@outlook.com>
2019-06-23 02:47:18 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -101,7 +101,6 @@ def can_destroy_a_single_item
   movie = Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie.delete 
   movie
-  __
 end
 
 def can_destroy_all_items_at_once
